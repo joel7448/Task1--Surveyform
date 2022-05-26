ec819f13b95fb1005887305badc3cade289fe945
A. Joel <103260485+joel7448@users.noreply.github.com>
2022-05-26 16:04:50 +0530

    Add files via upload

---
+++ 1sttask.js
@@ -0,0 +1,31 @@
+const stars1=document.querySelector("#k1");
+const stars2=document.querySelector("#k2");
+const stars3=document.querySelector("#k3");
+const stars4=document.querySelector("#k4");
+const stars5=document.querySelector("#k5");
+function star1(){
+    stars1.setAttribute("style","color:yellow");
+    
+}
+function star2(){
+    stars1.setAttribute("style","color:yellow");
+    stars2.setAttribute("style","color:yellow");   
+}
+function star3(){
+    stars1.setAttribute("style","color:yellow");
+    stars2.setAttribute("style","color:yellow"); 
+    stars3.setAttribute("style","color:yellow");
+}
+function star4(){
+    stars1.setAttribute("style","color:yellow");
+    stars2.setAttribute("style","color:yellow"); 
+    stars3.setAttribute("style","color:yellow");
+    stars4.setAttribute("style","color:yellow");
+}
+function star5(){
+    stars1.setAttribute("style","color:yellow");
+    stars2.setAttribute("style","color:yellow"); 
+    stars3.setAttribute("style","color:yellow");
+    stars4.setAttribute("style","color:yellow");
+    stars5.setAttribute("style","color:yellow");
+}
